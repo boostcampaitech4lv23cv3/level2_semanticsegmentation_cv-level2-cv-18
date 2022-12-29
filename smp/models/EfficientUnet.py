b36a96704ac9140eb358a0a7cd907d68aec17760
HyeongSeok Kim <tiryul@gmail.com>
2022-12-29 03:34:48 +0000

    update: EfficientUnet.py / add Efficientb3_Unet

--- smp/models/EfficientUnet.py
+++ smp/models/EfficientUnet.py
@@ -18,6 +18,23 @@ class Efficientb2_Unet(torch.nn.Module):
             'out' : x
         }
 
+class Efficientb3_Unet(torch.nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.segbackbone = smp.Unet(
+                                    encoder_name="efficientnet-b3", # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
+                                    encoder_weights="imagenet",     # use `imagenet` pre-trained weights for encoder initialization
+                                    in_channels=3,                  # model input channels (1 for gray-scale images, 3 for RGB, etc.)
+                                    classes=11,                     # model output channels (number of classes in your dataset)
+                                )
+        
+    def forward(self, x):
+        x = self.segbackbone(x)    
+
+        return {
+            'out' : x
+        }
+
 class Efficientb4_Unet(torch.nn.Module):
     def __init__(self) -> None:
         super().__init__()
